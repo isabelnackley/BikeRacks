de94d6b8c9919abc2e85ac84ed623d0567aa9cb4
Isabel Nackley <isabel.nackley@duke.edu>
2017-10-28 21:10:17 -0400

    Made some additions. Hopefully they helped?

--- BikeRacks/ViewController.swift
+++ BikeRacks/ViewController.swift
@@ -9,19 +9,34 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
-    
-    override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude: 36.0014, longitude: -78.9382, zoom: 14)
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
-        self.view = mapView
-    }
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
+    lazy var mapView = GMSMapView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let camera = GMSCameraPosition.camera(withLatitude: 36.0014, longitude: -78.9382, zoom: 14)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.last
+        let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude, zoom: 13.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        self.view = mapView
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
